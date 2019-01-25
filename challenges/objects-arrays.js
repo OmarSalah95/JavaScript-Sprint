6ac7a1b7c34ab4f38a64c5f70d928da15025b99e
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-25 09:32:45 -0800

    I feel like I am lagging but I am mostly done with onjects and arrays. I need to comeback for the last roar method to be applied to the prototype of dinosaur1

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -108,7 +108,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
+const animalNames = zooAnimals.forEach((animal) => {this.name = animal.animal_name, this.scientific = animal.scientific_name});
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -117,15 +117,15 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(animal => animal.animal_name.toLowerCase());
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -133,7 +133,7 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((acc, animal,) => acc + animal.population, 0);
 console.log(populationTotal);
 
 
